37d4a280d12ee9d73ba344d0441d6ffd421a63a5
Brik Royster <elkemore@gmail.com>
2018-12-15 23:43:58 -0800

    Basic UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,122 @@
 import {Component} from '@angular/core';
 
+interface InputValues {
+  time: {minutes: string; seconds: string; centiseconds: string;};
+  lag: {start: string; end: string;};
+  fadeouts: {baseCount: string; ttmCount: string;};
+}
+
+interface ActualValues {
+  time: {minutes: number; seconds: number; centiseconds: number;};
+  lag: {start: number; end: number;};
+  fadeouts: {baseCount: number; ttmCount: number;};
+}
+
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <img width="300" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  title = 'sm64-real-time';
+  ttmSlide = false;
+  strings: InputValues = {
+    time: {minutes: '', seconds: '', centiseconds: ''},
+    lag: {start: '', end: ''},
+    fadeouts: {baseCount: '', ttmCount: ''},
+  };
+  values: ActualValues = {
+    time: {minutes: 0, seconds: 0, centiseconds: 0},
+    lag: {start: 0, end: 0},
+    fadeouts: {baseCount: 0, ttmCount: 0},
+  };
+
+  updateMinutes(value: string) {
+    const num = Number.parseInt(value, 10);
+    if (Number.isNaN(num) || num < 0) {
+      this.strings.time.minutes = '0';
+      this.values.time.minutes = 0;
+    } else {
+      this.strings.time.minutes = String(num);
+      this.values.time.minutes = num;
+    }
+  }
+
+  updateSeconds(value: string) {
+    const num = Number.parseInt(value, 10);
+    if (Number.isNaN(num) || num < 0) {
+      this.strings.time.seconds = '00';
+      this.values.time.seconds = 0;
+    } else {
+      this.strings.time.seconds = this.padTime(num, true);
+      this.values.time.seconds = num;
+    }
+  }
+
+  updateCentiseconds(value: string) {
+    const num = Number.parseInt(value, 10);
+    if (Number.isNaN(num) || num < 0) {
+      this.strings.time.centiseconds = '00';
+      this.values.time.centiseconds = 0;
+    } else {
+      this.strings.time.centiseconds = this.padTime(num, false);
+      this.values.time.centiseconds = num;
+    }
+  }
+
+  updateLagStart(value: string) {
+    const num = this.formatCount(value);
+    this.strings.lag.start = String(num);
+    this.values.lag.start = num;
+  }
+
+  updateLagEnd(value: string) {
+    const num = this.formatCount(value);
+    this.strings.lag.end = String(num);
+    this.values.lag.end = num;
+  }
+
+  updateFadeoutsBase(value: string) {
+    const num = this.formatCount(value);
+    this.strings.fadeouts.baseCount = String(num);
+    this.values.fadeouts.baseCount = num;
+  }
+
+  updateFadeoutsTTM(value: string) {
+    const num = this.formatCount(value);
+    this.strings.fadeouts.ttmCount = String(num);
+    this.values.fadeouts.ttmCount = num;
+  }
+
+  toggleTTMSlide(value: boolean) {
+    this.ttmSlide = value;
+    if (!value) {
+      this.strings.fadeouts.ttmCount = '';
+      this.values.fadeouts.ttmCount = 0;
+    }
+  }
+
+  getRealTime(): string {
+    return `${this.values.time.minutes}:${
+        this.padTime(
+            this.values.time.seconds,
+            true)}.${this.padTime(this.values.time.centiseconds, false)}`;
+  }
+
+  private formatCount(value: string): number {
+    const num = Number.parseInt(value, 10);
+    if (Number.isNaN(num) || num < 0) {
+      return 0;
+    } else {
+      return num;
+    }
+  }
+
+  private padTime(value: number, lead: boolean): string {
+    const pad = value < 10 ? '0' : '';
+    if (lead) {
+      return pad + value;
+    } else {
+      return value + pad;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import {NgModule} from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppComponent} from './app.component';
+import {MaterialModule} from './material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FlexLayoutModule,
+    FormsModule,
+    MaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
