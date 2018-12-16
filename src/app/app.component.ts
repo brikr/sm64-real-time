5368a0e436054d4bb8efe968dd77f0f75c9ccd67
Brik Royster <elkemore@gmail.com>
2018-12-16 01:50:44 -0800

    remove some oddities with lag times
    lag start < lag end is no longer enforced.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -65,18 +65,12 @@ export class AppComponent {
 
   updateLagStart(value: string) {
     const num = this.formatCount(value);
-    if (num > this.values.lag.end) {
-      this.updateLagEnd(String(num));
-    }
     this.strings.lag.start = String(num);
     this.values.lag.start = num;
   }
 
   updateLagEnd(value: string) {
-    let num = this.formatCount(value);
-    if (num < this.values.lag.start) {
-      num = this.values.lag.start;
-    }
+    const num = this.formatCount(value);
     this.strings.lag.end = String(num);
     this.values.lag.end = num;
   }
